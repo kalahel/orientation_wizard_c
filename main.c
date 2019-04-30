30b335b7e73b757ad586181383cf55583cc6b8c9
henri <drheinrich940@gmail.com>
2019-04-30 23:12:28 +0200

    :sparkles: Created new structures and convertions functions. Added obstacles and environement.

--- main.c
+++ main.c
@@ -23,6 +23,19 @@ struct VectorPOLAR{
     double d_radius;
 }typedef VectorPOLAR;
 
+struct Obstacle{
+    int id;
+    PointGPS position;
+    double radius;
+}typedef Obstacle;
+
+struct Environment{
+    Obstacle * obstacles;
+    PointGPS destination;
+}typedef Environment;
+
+Environment environment;
+
 PointGPS createPoint(double longitude, double latitude){
     PointGPS point;
     point.latitude = latitude;
@@ -51,16 +64,16 @@ VectorPOLAR createVectorPOLAR(double d_angle, double d_radius){
     return vectorPolar;
 }
 
-VectorGPS gpsVectorPointAPointB(PointGPS pointA, PointGPS pointB){
+VectorGPS createGpsVectorFromPointGpsAPointGpsB(PointGPS pointA, PointGPS pointB){
     return createVectorGPS(pointB.longitude - pointA.longitude, pointB.latitude - pointA.latitude);
 }
 
-VectorGPS addGPSVectorAB(VectorGPS vectorA, VectorGPS vectorB){
+VectorGPS sumOfGpsVectorsAB(VectorGPS vectorA, VectorGPS vectorB){
     return createVectorGPS(vectorA.d_long + vectorB.d_long, vectorA.d_lat + vectorB.d_lat);
 }
 
-VectorPOLAR pointGPSA_pointGPSB_toPolarVector(PointGPS pointA, PointGPS pointB){
-    VectorGPS vectorGps = gpsVectorPointAPointB(pointA, pointB);
+VectorPOLAR createPolarVectorFromPointGpsAPointGpsB(PointGPS pointA, PointGPS pointB){
+    VectorGPS vectorGps = createGpsVectorFromPointGpsAPointGpsB(pointA, pointB);
     double radius = sqrt(vectorGps.d_lat * vectorGps.d_lat + vectorGps.d_long * vectorGps.d_long);
     double angle = 0.0;
     if(vectorGps.d_long >=0){
@@ -72,14 +85,67 @@ VectorPOLAR pointGPSA_pointGPSB_toPolarVector(PointGPS pointA, PointGPS pointB){
         }
     }else{
         if(vectorGps.d_lat >=0){
-            angle = (180.0 / M_PI) *(atan2(vectorGps.d_long, -vectorGps.d_lat));
+            angle = (180.0 / M_PI) *(atan2(vectorGps.d_long, -vectorGps.d_lat))+270;
         }else{
-            angle = (180.0 / M_PI) *(atan2(-vectorGps.d_long, -vectorGps.d_lat)) + 90;
+            angle = (180.0 / M_PI) *(atan2(-vectorGps.d_long, -vectorGps.d_lat)) + 180;
         }
     }
+    return createVectorPOLAR(angle, radius);
+}
+
+VectorPOLAR convertGpsVectorInPolarVector(VectorGPS vectorGps){
+    double radius = sqrt(vectorGps.d_lat * vectorGps.d_lat + vectorGps.d_long * vectorGps.d_long);
+    double angle = 0.0;
+    if(vectorGps.d_long >=0){
+        if(vectorGps.d_lat >=0){
+            // atan2 returns radians. To convert it in degrees we do the following :
+            angle = (180.0 / M_PI) *(atan2(vectorGps.d_long, vectorGps.d_lat));
+        }else{
+            angle = (180.0 / M_PI) *(atan2(-vectorGps.d_long, vectorGps.d_lat)) + 90;
+        }
+    }else{
+        if(vectorGps.d_lat >=0){
+            angle = (180.0 / M_PI) *(atan2(vectorGps.d_long, -vectorGps.d_lat))+270;
+        }else{
+            angle = (180.0 / M_PI) *(atan2(-vectorGps.d_long, -vectorGps.d_lat)) + 180;
+        }
+    }
+    return createVectorPOLAR(angle, radius);
+}
+
+VectorGPS convertPolarVectorInGpsVector(VectorPOLAR vectorPolar){
+    double longitude = vectorPolar.d_radius*    cos(vectorPolar.d_angle*(M_PI/180));
+    double latitude = vectorPolar.d_radius*     sin(vectorPolar.d_angle*(M_PI/180));
+    return createVectorGPS(longitude, latitude);
+}
+
+Obstacle createObstacle(int id, double radius, PointGPS position){
+    Obstacle obstacle;
+    obstacle.id = id;
+    obstacle.radius = radius;
+    obstacle.position = position;
+    return obstacle;
+}
+
+Environment createEnvironment(PointGPS destination, Obstacle* obstacles){
+    Environment environment;
+    environment.destination = destination;
+    environment.obstacles = obstacles;
+}
+
+
+
+VectorGPS computeDriverVectorFromEnvironement(){
+
 }
 
 
 int main() {
-
+    PointGPS pointGps1 = createPoint(49.202538, 6.918930);
+    PointGPS pointGps2 = createPoint(49.202650, 6.925839);
+    Obstacle obstacle1 = createObstacle(1, 10, pointGps1 );
+    Obstacle obstacle2 = createObstacle(2, 30, pointGps2 );
+    Obstacle* obstacles[2];
+    obstacles[0] = &obstacle1;
+    obstacles[1] = &obstacle2;
 }
